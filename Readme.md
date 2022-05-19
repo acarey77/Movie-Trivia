cf8217fc6369775c9280dcafe3de498af5202c83
andrescarey <andrescarey@gmail.com>
2022-05-19 09:12:36 -0400

    version 1.0

---
+++ Readme.md
@@ -0,0 +1,30 @@
+1. Create a div that holds the questions
+2. Create a button that requests de answer
+3. Create a div that contains the results
+4. Store the these elements in javascript variables through the DOM
+
+
+
+
+
+
+PHASE ONE: Complete
+
+1. Have a "start" button which will display de first question
+2. A question should be displayed together with 3 posible answers
+3. A circle should exist in the left of the answer text that when click should check answers
+4. When correct answer is checked the green box from that answer should be the only backgrounded element 
+5. If user chooses an incorrect answer, that box should turn into red and the correct box shoudl turn into green
+6. There should be a "next question" button to proceed to the next question 
+
+
+PHASE TWO
+
+1. Add a player score
+2. Add a reset button
+
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,114 @@
+const question = document.querySelector('#question')
+const answer1 = document.querySelector('p1')
+const answer2 = document.querySelector('p2')
+const correctAnswer = document.querySelector('#correct-answer')
+const incorrectAnswer1 = document.querySelector('#incorrect-answer1')
+const incorrectAnswer2 = document.querySelector('#incorrect-answer2')
+const result = document.querySelector('#result')
+const resetBtn = document.querySelector('#reset')
+let playerScore = document.querySelector('#score')
+let scoreCount = 0;
+
+playerScore.innerText = `PLAYER SCORE: ${scoreCount} / 3`
+
+
+const answerBoxes = document.querySelectorAll('.answer-boxes')
+// console.log(answerBoxes)
+
+const nextQuestion = document.querySelector('#next-question')
+// console.log(nextQuestion)
+
+
+data = [
+    question1 = {
+        question: "What was the name of Neo in the movie The Matrix?", 
+        incorrect1: 'Mr. Smith',
+        incorrect2: 'Captain Kangaroo',
+        correctanswer: "Mr. Anderson"
+    },
+
+    question2 = {
+        question: "What was the name of the little workers in Charlie and the Chocolate Factory?", 
+        incorrect1: 'Minions',
+        incorrect2: 'Smurfs',
+        correctanswer: "Oompa Loompas"
+    },
+
+    question3 = {
+        question: "What was Jordan Belfort's favorite drug in the Wolf of Wall Street?", 
+        incorrect1: 'Cocaine',
+        incorrect2: 'MDMA',
+        correctanswer: "Quaaludes"
+    },
+
+]
+
+let counter = 0
+
+
+question.innerText = data[0].question
+answer1.innerText = data[0].incorrect1
+answer2.innerText = data[0].incorrect2
+correctAnswer.innerText = data[0].correctanswer
+
+
+function correct() {
+    correctAnswer.style.backgroundColor = 'green'
+    result.innerText = 'Correct answer!'
+    scoreCount++
+    playerScore.innerText = `PLAYER SCORE: ${scoreCount} / 3`
+    incorrectAnswer1.removeEventListener('click', incorrect1);
+    incorrectAnswer2.removeEventListener('click', incorrect2);
+    ;
+}
+
+function incorrect1() {
+    incorrectAnswer1.style.backgroundColor = 'red'
+    result.innerText = 'Incorrect answer!'
+    incorrectAnswer2.removeEventListener('click', incorrect2);
+    correctAnswer.removeEventListener('click', correct);
+}
+
+function incorrect2() {
+    incorrectAnswer2.style.backgroundColor = 'red'
+    result.innerText = 'Incorrect answer!'
+    correctAnswer.removeEventListener('click', correct);
+    incorrectAnswer1.removeEventListener('click', incorrect1);
+}
+
+
+incorrectAnswer1.addEventListener('click', incorrect1);
+incorrectAnswer2.addEventListener('click', incorrect2);
+correctAnswer.addEventListener('click', correct);
+
+
+
+
+
+
+nextQuestion.addEventListener('click', () => {
+    counter++
+    question.innerText = data[counter].question
+    answer1.innerText = data[counter].incorrect1
+    answer2.innerText = data[counter].incorrect2
+    correctAnswer.innerText = data[counter].correctanswer
+
+    for (let i = 0; i < answerBoxes.length; i++) {
+        answerBoxes[i] = answerBoxes[i].style.backgroundColor = 'white'
+
+        incorrectAnswer1.addEventListener('click', incorrect1);
+        incorrectAnswer2.addEventListener('click', incorrect2);
+        correctAnswer.addEventListener('click', correct);
+
+        result.innerText = ''
+    }
+
+})
+
+resetBtn.addEventListener('click', () => {
+    window.location.reload();
+})
+
+
+
+
